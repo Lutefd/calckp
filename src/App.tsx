70d2c4754f85b74258022d7a808cc95ed4ac3fbb
Luis Dourado <lfaddourado@gmail.com>
2022-09-30 13:35:30 -0300

    define onChange event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -36,55 +36,89 @@ function App() {
   const [preVGROR, setPreVGROR] = useState(initialPreVGROR);
   const [preVGYield, setPreVGYield] = useState(initialPreVGYield);
   const [postVGROR, setPostVGROR] = useState(initialPostVGROR);
-  const [postVGYield, setPostVGYied] = useState(initialPostVGYield);
+  const [postVGYield, setPostVGYield] = useState(initialPostVGYield);
 
   return (
     <div className="App">
       <h1>Calculadora da Independencia Financeira</h1>
-      <h2>Você pode se aposentar com </h2>
-      <div>Quantidade de dinheiro necessária para aposentadoria: </div>
+      <h2>Você pode se aposentar em {retirementAge} anos</h2>
+      <div>
+        Quantidade de dinheiro necessária para aposentadoria: {retirementAmount}
+      </div>
       <br />
       <form className="calcForm">
         <label>
           Com qual salario deseja se aposentar?
-          <input type="number" />
+          <input
+            type="number"
+            value={retirementSalary}
+            onChange={(e) => setRetirementSalary(parseInt(e.target.value) || 0)}
+          />
         </label>
         <br />
         <label>
           Idade atual
-          <input type="number" />
+          <input
+            type="number"
+            value={currentAge}
+            onChange={(e) => setCurrentAge(parseInt(e.target.value) || 0)}
+          />
         </label>
         <br />
         <label>
           Patrimonio atual
-          <input type="number" />
+          <input
+            type="number"
+            value={currentAmount}
+            onChange={(e) => setCurrentAmount(parseInt(e.target.value) || 0)}
+          />
         </label>
         <br />
         <label>
           Aporte mensais
-          <input type="number" />
+          <input
+            type="number"
+            value={contributions}
+            onChange={(e) => setContributions(parseInt(e.target.value) || 0)}
+          />
         </label>
         <div>
           <h2>Ajustes das taxas de retorno para dev test</h2>
         </div>
         <label>
           Taxa de retorno sem VG
-          <input type="number" />
+          <input
+            type="number"
+            value={preVGROR}
+            onChange={(e) => setPreVGROR(parseInt(e.target.value) || 10.5)}
+          />
         </label>
         <br />
         <label>
           Yield sem VG
-          <input type="number" />
+          <input
+            type="number"
+            value={preVGYield}
+            onChange={(e) => setPreVGYield(parseInt(e.target.value) || 4)}
+          />
         </label>
         <br />
         <label>
           Taxa de retorno com VG
-          <input type="number" />
+          <input
+            type="number"
+            value={postVGROR}
+            onChange={(e) => setPostVGROR(parseInt(e.target.value) || 12.5)}
+          />
         </label>
         <br />
         <label>
           Yield com VG
-          <input type="number" />
+          <input
+            type="number"
+            value={postVGYield}
+            onChange={(e) => setPostVGYield(parseInt(e.target.value) || 8)}
+          />
         </label>
       </form>
     </div>
